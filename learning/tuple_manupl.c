2730f88159d74c00b035078ef02233157f37391c
soukanid <nidbellasoukaina.com>
2024-10-09 21:37:22 +0100

    see i said that from know on i will push every minute . cause i am still learning how to word with nvim so i need to be careful

---
+++ learning/tuple_manupl.c
@@ -0,0 +1,58 @@
+#include "Ray_tracing.h"
+
+tuple new_tuple(float x, float y, float z, float w)
+{
+  tuple new;
+
+  new.x = x;
+  new.y = y;
+  new.z = z;
+  new.w = w;
+  return (tuple);
+}
+
+tuple add_tuple(tuple t1, tuple t2)
+{
+  return (new_tuple(t1.x + t2.x,
+                    t1.y + t2.y,
+                    t1.z + t2.z,
+                    t1.w + t2.w);
+}
+
+
+tuple add_tuple(tuple t1, tuple t2)
+{
+  return (new_tuple(t1.x - t2.x,
+                    t1.y - t2.y,
+                    t1.z - t2.z,
+                    t1.w - t2.w);
+}
+
+
+tuple multiply_tuple(tuple t1, tuple t2)
+{
+  return (new_tuple(t1.x * t2.x,
+                    t1.y * t2.y,
+                    t1.z * t2.z,
+                    t1.w * t2.w);
+}
+
+
+tuple divide_tuple(tuple t1, tuple t2)
+{
+  return (new_tuple(t1.x / t2.x,
+                    t1.y / t2.y,
+                    t1.z / t2.z,
+                    t1.w / t2.w);
+}
+
+
+tuple negate_tuple(tuple t)
+{
+  return (new_tuple(-t.x,
+                    -t.y,
+                    -t.z,
+                    -t.w);
+}
+
+
