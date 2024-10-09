5331aef963242e411644b61f49c444db9e979c63
soukanid <nidbellasoukaina.com>
2024-10-09 22:47:33 +0100

    i did warn you i will really push every 30 min HHHHHHHHHHHHHHHH

--- learning/tuple_manupl.c
+++ learning/tuple_manupl.c
@@ -55,4 +55,27 @@ tuple negate_tuple(tuple t)
                     -t.w));
 }
 
+float magnitude(tuple t)
+{
+  return (sqrt(pow(t.x, 2) + pow(t.y, 2) + pow(t.z, 2)));
+}
 
+tuple normalize_vector(tuple t)
+{
+  float magn;
+
+  magn = magnitude(t);
+  return (new_tuple(t.x / magn, t.y / magn,
+          t.z / magn, 0));
+}
+ float dot(tuple t1 ,tuple t2)
+{
+  return (t1.x * t2.x + t1.y * t2.y + t1.z * t2.z);
+}
+
+tuple cross_product(tuple t1, tuple t2)
+{
+  return (new_tuple(t1.y * t2.z - t1.z * t2.y,
+                    t1.z * t2.x - t1.x * t2.z,
+                    t1.x * t2.y - t1.y * t2.x, 0));
+}
